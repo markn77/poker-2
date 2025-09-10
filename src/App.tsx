fca97ea160db6b33f4a6e46e55928304677ad63a
Fatih <fhi5854@gmail.com>
2025-09-10 17:38:07 -0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Hello Tailwind 🚀
+    </div>
+  );
+}
+
+export default App;
